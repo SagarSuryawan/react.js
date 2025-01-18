c7154669961238a01d7abdedfb84e0165fb2c436
Sagar <sagarsurya45@gmail.com>
2025-01-18 19:02:28 +0530

    data fetch detail theory added,pagination is going on

--- react_learning_part_3/pokedex/src/components/PokemonList/PokemonList.jsx
+++ react_learning_part_3/pokedex/src/components/PokemonList/PokemonList.jsx
@@ -8,20 +8,23 @@ function PokemonList (){
     const [PokemonList ,setPokemonList] = useState([]) //1 
     const [isLoading , setisLoading] = useState(true) //2
 
+    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon'
+
     async function fetchPokemons(){
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon')
+        const response = await axios.get(POKEDEX_URL) //this fetch list of 20 pokemons
 
-        const pokemonResult = response.data.results   // results key of object having pokemons
+        const pokemonResult = response.data.results   // we get the array of pokemons,this array has name and url,url for details of pokemons.
 
-        const pokemonResultPromise = pokemonResult.map((pokemon) =>axios.get(pokemon.url))  //every pokemon has name and url,wrap in array
+        const pokemonResultPromise = pokemonResult.map((pokemon) =>axios.get(pokemon.url))  //iterating over the array of pokemons, and using their url to create array of promises,that will download those 20 pokemons.
         
         const pokemonData = await axios.all(pokemonResultPromise)
+       //passing that promise array to axios.all and this is array of pokemons detailed data.
 
         console.log(pokemonData)
 
         //next is to save in PokemonList //1
 
-        const res = (pokemonData.map((pokeData) => {
+        const res = (pokemonData.map((pokeData) => { //iterate on each the data of each pokemon and exract id,names,image and types.
             const pokemon = pokeData.data
 
             return {
