84d496551e10b844ff508eca49362b5267d52c68
Sagar <sagarsurya45@gmail.com>
2025-03-23 19:28:19 +0530

    crateStroe(dispatch,subscribe,replacereducer and getState)

--- redux/demoCreateStore.js
+++ redux/demoCreateStore.js
@@ -1,20 +1,18 @@
-import {createStore}  from redux
+import {createStore}  from 'redux'
 
-function todoReducer(state, action) {
+function todoReducer(state= [], action) {
 
     if(action.type == "add_Todo"){
-        const todoData = action.payload.todoText
+        const todoText = action.payload.todoText
         return[
             ...state, { 
-                todoData:todoText,
-                isFinished:false,
-                id:(state.length == 0) ? 1 : state([state.length -1].id+1)
+                todoData:todoText, isFinished:false, id:(state.length == 0) ? 1 : state[state.length -1].id+1
 
             }
         ]
     }else if(action.type == "delete_todo"){
-        const todo = action.payload.todo
-        return state.filter(t => t.id != todo.id)
+        const todoid = action.payload.todoid
+        return state.filter(t => t.id != todoid)
     }
     else if(action.type == "edit_Text"){
         const todo = action.payload.todo;
@@ -23,12 +21,39 @@ function todoReducer(state, action) {
             if(t.id == todo.id){
                 todoData = todoText
             }
+            return t;
         })
 
-        return t;
     }
+
     else state
+    
 }
 
 
-const response = createStore(todoReducer, [])
+const {response} = createStore(todoReducer, [])
+
+// destructure the response 
+const {dispatch, subscribe, getState, replaceReducer} = createStore(todoReducer , [])
+
+subscribe(()=> console.log("Updating",getState()))
+
+
+dispatch({type: 'add_Todo', payload: {todoText: 'todo 1'}})
+dispatch({type: 'add_Todo', payload: {todoText: 'todo 2'}})
+// dispatch({type: 'add_Todo', payload: {todoText: 'todo 3'}})
+// dispatch({type: 'add_Todo', payload: {todoText: "todo 4"}})
+
+// console.log("for adding",getState())
+
+
+
+
+
+dispatch({type : "delete_todo" , payload: {todoid: 1}})
+// dispatch({type : "delete_todo" , payload: {todoid: 4}})
+// console.log("for deleteing",getState())
+
+// console.log("for checking",getState())
+
+
---
+++ redux/readme.md
@@ -0,0 +1,14 @@
+# What is Redux?
+Redux is a state management library for JavaScript applications, commonly used with React. It helps manage and share state across the app in a predictable way.
+
+Instead of passing props manually through multiple levels (prop drilling), Redux stores global state in a central store, making it accessible from anywhere in the application.
+
+# what is compose method ? 
+
+
+
+
+
+# what is createMethod method ? 
+
+
