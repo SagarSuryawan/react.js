5c34ee77e2d99fc5560a6dda526665e11a02483a
Sagar <sagarsurya45@gmail.com>
2025-04-03 12:16:23 +0530

    added login signup btn to sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,9 +1,21 @@
 import {FiMenu}  from 'react-icons/fi'
 import {AiFillCloseCircle} from 'react-icons/ai'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import Footer from '../Components/Footer'
+import {useDispatch, useSelector}  from 'react-redux'
 
 function HomeLayout( {children} ) {
+
+    const dispatch = useDispatch()
+
+    const navigate = useNavigate()
+    // used for navigating between pages without relying on <Link> or <Redirect>. You can pass data while navigating
+
+    const isLoggedIn = useSelector((state) => state ?.auth ?.isLoggedIn) 
+    const role = useSelector((state) => state?. auth?.role)
+    //If state.auth does not exist, it returns undefined instead of crashing.
+    // if state not null then return auth and auth not null then isLoggedIn return
+
 
     function ChangeWidth () {
         // This function tries to set the width of the sidebar (.drawer-side) to 0.
@@ -24,8 +36,18 @@ function HomeLayout( {children} ) {
         
     }
 
+    async function handelLogout(e) {
+        e.preventDefault()
+
+        // const res = await dispatch(logout())
+        // logout get from auth slice
+
+        // if(res?.payload?.success)
+            navigate("/")
+    }
+
     return(
-        <div className="min-h-[90vh]  ">
+        <div className="min-h-[100vh]  ">
 
             <div className="drawer absolute left-0 z-50 w-fit ">
                 <input type="checkbox" id="my-drawer" className="drawer-toggle"  />
@@ -52,6 +74,11 @@ function HomeLayout( {children} ) {
                             <Link to="/">Home</Link>
 
                         </li>
+                        {isLoggedIn && role === "ADMIN" && (
+                            <li>
+                                 <Link to="/admin/dashboard">Admin Dashboard</Link>
+                            </li>
+                        )}
                         <li>
                             <Link to="/courses">All Courses</Link>
                             
@@ -64,6 +91,33 @@ function HomeLayout( {children} ) {
                             <Link to="/about">About Us</Link>
                             
                         </li>
+                        {!isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                            <div className='w-full flex items-center justify-center gap-2'>
+                                <button className='border border-yellow-500  hover:bg-yellow-600 btn-primary px-4 py-1 font-semibold rounded-md w-full transition-all ease-in-out duration-300'>
+                                    <Link to= "/Login">Login</Link>
+                                </button>
+
+                                <button className='border border-yellow-500 bg-yellow-500   btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                    <Link to= "/signup">Signup</Link>
+                                </button>
+                            </div>
+                            </li>
+                        )}
+
+                        {isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                            <div className='w-full flex items-center justify-center gap-2'>
+                                <button className='border border-yellow-500  hover:bg-yellow-600 btn-primary px-4 py-1 font-semibold rounded-md w-full transition-all ease-in-out duration-300'>
+                                    <Link to= "/user/profile">Profile</Link>
+                                </button>
+
+                                <button className='border border-yellow-500 bg-yellow-500   btn-secondary px-4 py-1 font-semibold rounded-md w-full'>
+                                    <Link onClick={handelLogout}>Logout</Link>
+                                </button>
+                            </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
